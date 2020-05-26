0dfe1c3ba99af134e41545e80023224f368e986c
zmichaela <m.zajacova90@gmail.com>
2020-05-26 16:20:21 +0100

    test

--- js/script.js
+++ js/script.js
@@ -4,6 +4,6 @@ window.onload = function() {
     document.getElementById("name").focus();
   };
   
-//job role section
+//job role section TEST
 You'll need to add the "Other" job role input directly into the HTML and hide it initially with JS in order to get this feature 
 to work when JS is disabled, which is a requirement below.
